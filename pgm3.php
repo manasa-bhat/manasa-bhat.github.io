7c645188775a269272fdb0e41107a946cff48541
phygital <manasasirsi17@gmail.com>
2016-09-12 13:33:17 +0530

    second commit

--- pgm3.php
+++ pgm3.php
@@ -1,8 +1,38 @@
 <html>
 <body>
 <?php
-echo "firstname=$a";
-echo "lastname=$b";
+echo "firstname ";
+echo "lastname ";
+$firstname="manasa";
+$lastname="bhat";
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
+$con=mysqli_connect("localhost","root","admin@123","test");
+if(!$con)
+ {
+  die('could not connect:' .mysqli_connect_error());
+ }
+mysqli_select_db("test",$con);
+
+
+
+$sql="insert into nametable(firstname,lastname)values
+   ('$firstname','$lastname')";
+
+if(!mysqli_query($con,$sql))
+ {
+   echo("Error description: " . mysqli_error($con));
+ }
+
+/*if (!mysqli_query($con,"INSERT INTO Persons (FirstName) VALUES ('Glenn')"))
+  {
+  echo("Error description: " . mysqli_error($con));
+  }*/
+
+echo "1 record added";
+mysqli_close($con);
+
+
 ?>
 </body>
 </html>
