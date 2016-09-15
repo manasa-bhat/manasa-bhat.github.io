5b4bbb860aa1602c51cb37c3b9a6a4aeb2e2d26c
phygital <manasasirsi17@gmail.com>
2016-09-15 15:56:26 +0530

    initial commit

---
+++ pgm8.php
@@ -0,0 +1,99 @@
+<html>
+<body>
+<h1> a small example page to search  some data from the MYSQL USING PHP</h1>
+
+<form action ="pgm8.php" method="post">
+search:<input type="text" name="search">
+
+<select name="column">
+  <option value="firstname">firstname</option>
+  <option value="lastname">lastname</option>
+  <option value="usn">usn</option>
+  <option value="address">address</option>
+  <option value="university">university</option>
+  <option vlue="datetime">datetime</option>
+</select> 
+<input type="submit" value="go">
+</form>
+
+
+
+
+<?php
+echo "hi";
+$search= "%".$_POST['search']."%" ;
+echo $search;
+$column=$_POST['column'];
+
+
+$con=mysqli_connect("localhost","root","admin@123","newtest");
+if(!$con)
+ {
+  die('could not connect:' .mysqli_connect_error());
+ }
+
+if($column == 'firstname') { 
+    $query = "SELECT * FROM student  WHERE firstname LIKE '$search' "; 
+echo $query;
+}
+
+else if($column == 'lastname') { 
+    $query = "SELECT COUNT(*) FROM student  WHERE lastname like '$search'"; 
+}
+else if($column == 'usn') { 
+    $query = "SELECT COUNT(*) FROM student  WHERE usn like '$search'"; 
+}
+else if($column == 'address') { 
+    $query = "SELECT COUNT(*) FROM student  WHERE address like '$search'"; 
+}
+else if($column == 'university') { 
+    $query = "SELECT COUNT(*) FROM student  WHERE university like '$search'"; 
+}
+else if($column == 'datetime') { 
+    $query = "SELECT COUNT(*) FROM student  WHERE datetime like '$search'"; 
+}
+
+else
+  {
+   $query= "select * from student";
+  print_r($query);
+  }
+
+if(!mysqli_query($con,$query))
+
+ {
+   echo("Error description: ".mysqli_error($con));
+
+ }
+
+
+else
+{
+//echo $query;
+$result = mysqli_query($con, $query);
+
+while ($row = mysqli_fetch_array($result,MYSQLI_NUM)) 
+{
+print_r($row);
+printf ("%s (%s)(%S)(%S)(%S)(%S)\n",$row[0],$row[1],$row[2],$row[3],$row[4],$row[5]);
+}
+
+
+
+/*while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) 
+{
+
+printf ("%s (%s)(%S)(%S)(%S)(%S)\n",$row["firstname"],$row["lastname"],$row["usn"],$row["address"],$row["university"],$row["datetime"]);
+}*/
+
+
+}
+
+mysqli_close($con);
+
+
+
+?>
+</body>
+</html>
+
